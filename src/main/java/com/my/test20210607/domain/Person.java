c5bf0b15ec89a25dccf503db6425c0fbc930af33
yuzixiang <995755801@qq.com>
2021-06-08 09:17:10 +0800

    111

---
+++ src/main/java/com/my/test20210607/domain/Person.java
@@ -0,0 +1,15 @@
+package com.my.test20210607.domain;
+
+/**
+ * ClassName:Person
+ * Package:com.my.test20210607.domain
+ * Description：
+ *
+ * @Date：2021/6/7 21:59
+ * @Author：yuzixiang
+ */
+public class Person {
+
+    private String name;
+
+}
